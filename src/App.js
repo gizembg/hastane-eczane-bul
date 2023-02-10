2932f2eb1fcb343169d7bd0b0f4f350712733498
DarkSavci <darksavci@gmail.com>
2023-02-10 08:04:19 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,99 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import MarkerClusterGroup from "react-leaflet-markercluster";
+import "./App.css";
+import Box from "@mui/material/Box";
+import useFetch from "./useFetch";
+import eczanedb from "./denemedb";
+import centers from "./cityCenters";
+
+function CityComponent(props) {
+  const map = useMap();
+
+  return (
+    <Box
+      sx={{
+        cursor: "pointer",
+        backgroundColor: "white",
+        borderRadius: "5px",
+        textAlign: "center",
+      }}
+      onClick={() => {
+        map.flyTo([props.lat, props.long], 12);
+      }}
+    >
+      {props.city}
+    </Box>
+  );
+}
+
+export default function App() {
+  const { data, loading, error } = useFetch("http://v2.jokeapi.dev/joke/Any");
+  if (loading) return <h1>Yükleniyor</h1>;
+  if (error) console.log(error);
+
+  return (
+    <Box>
+      {
+        //Eğer mapin üzerine bir şey eklemek isterseniz buraya ekleyin alttaki kutu bir örnektir uncomment edip bakabilirsiniz
+      }
+
+      {/*<Box>
+        Bu bir örnek
+    </Box>*/}
+
+      <MapContainer
+        className="hazir-map" //class adı kendinize göre ayarlayabilirsiniz isterseniz
+        center={[37.683664, 38.322966]} //CENTER BILGINIZ NEREDE İSE ORAYA KOYUNUZ
+        zoom={7} //ZOOM NE KADAR YAKINDA OLMASINI
+        maxZoom={17}
+        //maxZoomu kendinize göre ayarlayın
+      >
+        
+        <Box
+          sx={{
+            position: "absolute",
+            marginTop: "5px",
+            top: 0,
+            right: 20,
+            zIndex: 1000,
+            gap: "2px",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          {centers.map((data) => {
+            return (
+              <CityComponent {...data} />
+            );
+          })}
+        </Box>
+
+
+        <TileLayer //Bu kısımda değişikliğe gerek yok
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+
+        <MarkerClusterGroup>
+          {eczanedb
+            //Bu kısmı değiştirin kendi datanıza göre stations.map kısmını kendi datanıza göre mydata.map gibi
+            .map((station) => {
+              return (
+                <Marker
+                  key={station.id} //key kısmını da kendi datanıza göre ayarlayın mydaya.id gibi
+                  position={[station.latitude, station.longitude]} //Kendi pozisyonunuzu ekleyin buraya stationı değiştirin mydata.adress.latitude mydata.adress.longitude gibi
+                >
+                  <Popup>
+                    {
+                    // Markerın üzerine tıklandığında açılacak olan kutu burası
+                    }
+                  </Popup>
+                </Marker>
+              );
+            })}
+        </MarkerClusterGroup>
+      </MapContainer>
+    </Box>
+  );
+}
---
+++ src/cityCenters.js
@@ -0,0 +1,54 @@
+const centers = [
+    {
+        city: "Adana",
+        lat: 36.9914,
+        long: 35.3308,
+    },
+    {
+        city: "Adıyaman",
+        lat: 37.7636,
+        long: 38.2773,
+    },
+    {
+        city: "Diyarbakır",
+        lat: 37.925,
+        long: 40.211,
+    },
+    {
+        city: "Hatay",
+        lat: 36.2023,
+        long: 36.1613,
+    },
+    {
+        city: "Gaziantep",
+        lat: 37.066,
+        long: 37.3781,
+    },
+    {
+        city: "Kahramanmaraş",
+        lat: 37.5753,
+        long: 36.9228,
+    },
+    {
+        city: "Kilis",
+        lat: 36.7165,
+        long: 37.1147,
+    },
+    {
+        city: "Malatya",
+        lat: 38.3554,
+        long: 38.3335,
+    },
+    {
+        city: "Şanlıurfa",
+        lat: 37.1674,
+        long: 38.7955,
+    },
+    {
+        city: "Osmaniye",
+        lat: 37.0746,
+        long: 36.2464,
+    },
+];
+
+export default centers;
