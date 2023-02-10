f0110e890e79839513be5268f63809b352866754
TCGBOGA <gizem.boga@turkcell.com.tr>
2023-02-11 02:18:47 +0300

    feat: servisler düzenlendi

--- src/components/MainViewContainer.js
+++ src/components/MainViewContainer.js
@@ -21,6 +21,7 @@ import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import LocationOnIcon from '@mui/icons-material/LocationOn'
 import CallIcon from '@mui/icons-material/LocationOn'
+import axios from 'axios';
 
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -36,10 +37,10 @@ const MainViewContaier = () => {
     const dispatch = useDispatch()
 
 
-    
-    
 
- 
+
+
+
 
     const [alignment, setAlignment] = React.useState('harita');
     const [alignmentFilter, setAlignmentFilter] = React.useState('hepsi');
@@ -78,12 +79,55 @@ const MainViewContaier = () => {
         }
     });
 
+    // //    const { data, loading, error } = useFetch("https://apieczane.afetharita.com/api/cityWithDistricts ");
 
-    const { data, loading, error } = useFetch("https://apieczane.afetharita.com/api/cityWithDistricts ");
-        if (loading) return <h1>Yükleniyor</h1>;
-        if (error) console.log(error);
+
+    // const { data, loading, error } = useFetch("https://apieczane.afetharita.com/api");
+    // const { data2, loading2, error2 } = useFetch("https://apieczane.afetharita.com/api/cityWithDistricts");
+
+    // // if (loading) {
+    // //     return <h1>Yükleniyor</h1>;
+
+    // // }
+    // if (error) {
+    //     console.log(error);
+    // }
+    // console.log("data", data)
+    // if (loading2) {
+    //     return <h1>Yükleniyor</h1>
+    // }
+    // if (error2) { console.log(error2); }
+    // console.log("data2", data2)
+
+    const [data, setData] = React.useState(null)
+    const [citydata, setCityData] = React.useState(null)
+
+    useEffect(() => {
+        axios.get("https://apieczane.afetharita.com/api").then((response) => {
+            setData(response.data);
+        }).catch((err) => {
+            //setError(err)
+        })
+
+    }, []);
+
+    useEffect(() => {
+        axios.get("https://apieczane.afetharita.com/api/cityWithDistricts ").then((response) => {
+            setCityData(response.data);
+        }).catch((err) => {
+            //setError(err)
+        })
+
+    }, []);
+
+    useEffect(()=>{
         console.log("data", data)
- 
+        console.log("citydata",citydata)
+
+    },[data, citydata])
+
+
+    
     return (
 
         <Paper id='fullheight' sx={{ bgcolor: '#182151', height: '100%', padding: '30px 200 100 200' }} variant="outlined" >
@@ -185,48 +229,89 @@ const MainViewContaier = () => {
 
                 }}>
                     <MapContainer
-                        className="hazir-map" //class adı kendinize göre ayarlayabilirsiniz isterseniz
-                        center={[37.683664, 38.322966]} //CENTER BILGINIZ NEREDE İSE ORAYA KOYUNUZ
-                        zoom={7} //ZOOM NE KADAR YAKINDA OLMASINI
+                        className="hazir-map"    //class adı kendinize göre ayarlayabilirsiniz isterseniz
+                        center={[37.683664, 38.322966]}    //CENTER BILGINIZ NEREDE İSE ORAYA KOYUNUZ
+                        zoom={7}                 //ZOOM NE KADAR YAKINDA OLMASINI 
                         maxZoom={17}
                     //maxZoomu kendinize göre ayarlayın
                     >
-
-                        <Box
-                            sx={{
-                                position: "absolute",
-                                marginTop: "5px",
-                                top: 0,
-                                right: 20,
-                                zIndex: 1000,
-                                gap: "2px",
-                                display: "flex",
-                                flexDirection: "column",
-                            }}
-                        >
-                        </Box>
-
-
-                        <TileLayer //Bu kısımda değişikliğe gerek yok
+                        <TileLayer    //Bu kısımda değişikliğe gerek yok
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+
+
                         />
 
+
                         <MarkerClusterGroup>
-                            {eczanedb
+                            {data?.data.
                                 //Bu kısmı değiştirin kendi datanıza göre stations.map kısmını kendi datanıza göre mydata.map gibi
-                                .map((station) => {
+                                map((station) => {
+
+                                    var adresFormatted = station.address.split(", Türkiye")[0].split(",").pop().split(" ")
+                                    var addres = adresFormatted[adresFormatted.length - 1]
                                     return (
                                         <Marker
                                             key={station.id} //key kısmını da kendi datanıza göre ayarlayın mydaya.id gibi
-                                            position={[station.latitude, station.longitude]} //Kendi pozisyonunuzu ekleyin buraya stationı değiştirin mydata.adress.latitude mydata.adress.longitude gibi
+                                            position={[station.latitude, station.longitude]}//Kendi pozisyonunuzu ekleyin buraya stationı değiştirin mydata.adress.latitude mydata.adress.longitude gibi
                                         >
+
                                             <Popup>
-                                                {
-                                                    // Markerın üzerine tıklandığında açılacak olan kutu burası
-                                                }
+
+
+                                                <Box>
+                                                    <Stack width="400px" >
+                                                        <Box>
+                                                            <Stack direction="row" spacing={0} margin="0px" padding="0px 12px">
+                                                                <Box padding="3px 4px 0px 0px">
+                                                                    <img src="https://cdn.discordapp.com/attachments/1073402852644499537/1073699707299315803/pill.png" width="16px" height="16px" ></img>
+                                                                </Box>
+                                                                <Typography margin="0px" color={"#F83B3B"}>
+                                                                    Eczane
+                                                                </Typography>
+                                                            </Stack>
+                                                        </Box>
+                                                        <Typography fontSize="24px" p="0px 12px">{station.name}</Typography>
+
+                                                        <Divider style={{ width: '93.5%', margin: "7px 4px" }} />
+
+                                                        <Stack direction="column">
+
+
+                                                            <Stack direction="row" padding="3px 0px 0px 7px" justifyContent="space-evenly">
+                                                                <Box sx={{ display: "flex", }}>
+                                                                    <LocationOnIcon ></LocationOnIcon>
+                                                                    <Typography display="flex" padding="3px">{
+                                                                        addres
+                                                                    }</Typography>
+                                                                </Box>
+
+                                                                <Box sx={{ display: "flex" }}>
+
+                                                                    <CallIcon></CallIcon>
+                                                                    <Typography>{station.phone}</Typography>
+                                                                </Box>
+
+
+                                                            </Stack>
+
+
+                                                            <Typography padding="0px 12px" display="absolute" color="#182151">{station.address}</Typography>
+
+                                                        </Stack>
+
+
+                                                    </Stack>
+
+                                                </Box>
+
+
                                             </Popup>
+
+
+
                                         </Marker>
+
                                     );
                                 })}
                         </MarkerClusterGroup>
@@ -238,12 +323,12 @@ const MainViewContaier = () => {
 
             <Box sx={{ flexGrow: 1, marginTop: '30px', height: '500px', overflow: 'auto', textAlign: 'center' }}>
                 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                    {data?.data.map((city, index) => (
+                    {citydata?.data.map((city, index) => (
                         <Grid item xs={2} sm={4} md={4} key={index}>
                             <Button variant="outlined">{`${city.key}`}</Button>
                         </Grid>
                     ))}
-                </Grid>
+                </Grid> 
             </Box>
 
             {alignment === 'liste' &&
